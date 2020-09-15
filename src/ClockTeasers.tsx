5984533eedc250f6348746ab87745febe8cc017d
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2020-09-15 07:55:39 +0200

    Random poster image.

--- src/ClockTeasers.tsx
+++ src/ClockTeasers.tsx
@@ -14,6 +14,7 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       flexGrow: 1,
+      marginTop: "50px",
     },
     "@keyframes fadeAnimation": {
       from: { opacity: 0 },
