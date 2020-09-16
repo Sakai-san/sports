4845ad51f70b37d1c55a13b937f45e71ba2886f9
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2020-09-16 10:11:18 +0200

    Updates

---
+++ precache-manifest.8e1628190c73671e1b31ae647ba04134.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b8c2cc3b8f12d18c23cf313682b2e08",
+    "url": "/sports/index.html"
+  },
+  {
+    "revision": "84b4aba8a4481df26a8c",
+    "url": "/sports/static/css/main.1c5056d7.chunk.css"
+  },
+  {
+    "revision": "afc1e7ce7cec5d5ab13a",
+    "url": "/sports/static/js/2.d37a9507.chunk.js"
+  },
+  {
+    "revision": "b948d2a7727b83687a00ec53ef5267c0",
+    "url": "/sports/static/js/2.d37a9507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84b4aba8a4481df26a8c",
+    "url": "/sports/static/js/main.b6ac439a.chunk.js"
+  },
+  {
+    "revision": "9b87600bb0ce04591fee",
+    "url": "/sports/static/js/runtime-main.6b867c65.js"
+  }
+]);
